66013ff8e0e02b078a91bb4a2a071cfd7ca30b12
Ítalo <italo.lemos@reitoria.ifpe.edu.br>
2021-02-18 19:50:28 -0300

    Avoiding duplicate actions

--- actions/models.py
+++ actions/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+
 class Action(models.Model):
     user = models.ForeignKey('auth.User', related_name='actions', db_index=True,
                              on_delete=models.CASCADE)
@@ -10,9 +11,9 @@ class Action(models.Model):
     target_ct = models.ForeignKey(ContentType, blank=True, null=True,
                                   related_name='target_obj',
                                   on_delete=models.CASCADE)
-    target_id = models.PositiveIntegerField(null=True, blank=True, db_index=True)
+    target_id = models.PositiveIntegerField(null=True, blank=True,
+                                            db_index=True)
     target = GenericForeignKey('target_ct', 'target_id')
-
 
     class Meta:
         ordering = ('-created',)
